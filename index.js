25afd1b3f02ba8bc4b497a48933fb5f90bee4016
lg4tin <lg4tin@gmail.com>
2023-09-22 19:43:48 -0400

    Add function to add X and Os to the board

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const gameboard = (() => {
   let board = [
-    ['x', 'o', 'x',],
-    ['x', 'o', 'x',],
-    ['x', 'o', 'x',]
+    '', '', '',
+    '', '', '',
+    '', '', ''
   ];
 
   let boardDivs = document.querySelectorAll('.row div')
 
   const displayBoard = () => {
     for (let i in boardDivs) {
-      boardDivs[i].textContent = board[i][i];
+      boardDivs[i].textContent = board[i];
+      boardDivs[i].addEventListener('click', () => {
+        if (controlFlow.turn === 'X') {
+          boardDivs[i].innerText = 'X';
+          controlFlow.turn = 'O';
+        } else {
+          boardDivs[i].innerText = 'O';
+          controlFlow.turn = 'X';
+        }
+      })
     }
   }
   return {displayBoard};
 })();
+
+const controlFlow = (() => {
+  let turn = 'X';
+
+  
+
+  return {turn};
+})();
+
+const Player = (name) => {
+  const playerName = name;
+
+
+}
 
 gameboard.displayBoard()
