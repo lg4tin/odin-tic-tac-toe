7caf62253dcda322929da7d00d654ee604fc67f5
lg4tin <lg4tin@gmail.com>
2023-09-22 20:53:47 -0400

    Add eventlistener to restart button to restart game

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let turnHeader = document.querySelector('.turn');
 let x = document.querySelector('.x');
 let o = document.querySelector('.o');
+let restart = document.querySelector('.restart');
 
 x.addEventListener('click', () => {
   controlFlow.turn = 'X';
@@ -10,6 +11,10 @@ x.addEventListener('click', () => {
 o.addEventListener('click', () => {
   controlFlow.turn = 'O';
   turnHeader.innerText = `${controlFlow.turn}'s Turn`;
+})
+
+restart.addEventListener('click', () => {
+  gameboard.clearBoard();
 })
 
 const gameboard = (() => {
@@ -40,11 +45,24 @@ const gameboard = (() => {
       })
     }
   }
-  return {displayBoard};
+
+  const clearBoard = () => {
+    for (let i in boardDivs) {
+      boardDivs[i].textContent = '';
+    }
+  }
+
+  return {displayBoard, clearBoard};
 })();
 
 const controlFlow = (() => {
   let turn = 'X';
+
+  const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
 
   return {turn};
 })();
